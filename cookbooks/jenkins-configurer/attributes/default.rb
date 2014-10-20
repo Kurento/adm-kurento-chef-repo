31751b15a885a6ba7279a6e9dd706d2a535b603b
Javier Lopez <fjlopez@gsyc.es>
2014-10-20 13:57:13 +0200

    Remove old recipes
    Change-Id: I20c9f39cf0a0870d8735d10961ba80d966a37e77

--- cookbooks/jenkins-configurer/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['jenkins-configurer']['user'] = "jenkins"
-default['jenkins-configurer']['group'] = "jenkins"
-default['jenkins-configurer']['home'] = "/var/lib/jenkins"
-default['jenkins-configurer']['email'] = "jenkins@acme.org"
-default['jenkins-configurer']['master-host'] = "ci.acme.org"
