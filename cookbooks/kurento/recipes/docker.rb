1cade22252c7db4d47f0fa50a38cc54b414db879
gortazar <patxi.gortazar@gmail.com>
2015-09-14 08:32:06 +0200

    Docker recipe migrated to latest docker (1.8)
    Change-Id: Ibc0f3d69462dc4a82fe27983684eabaa9a5fdc4e

--- cookbooks/kurento/recipes/docker.rb
+++ cookbooks/kurento/recipes/docker.rb
@@ -17,74 +17,16 @@
 # limitations under the License.
 #
 
-# Install docker v>=1 with remote access to service on port 20023
-execute "wget -qO- https://get.docker.io/gpg | sudo apt-key add - && touch /tmp/docker-key" do
-	not_if { ::File.exists?("/tmp/docker-key")}
+group 'docker' do
+	members node['kurento']['user']
 end
 
-execute "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list " do
-	not_if { ::File.exists?("/etc/apt/sources.list.d/docker.list")}
-end
+# Install docker-engine
+package 'curl'
+execute 'curl -sSL https://get.docker.com/ | sh'
 
-execute 'apt-get update'
-
-package 'docker.io' do
-	ignore_failure true
-end
-
-execute 'ln -sf /usr/bin/docker.io /usr/local/bin/docker'
-
-ruby_block "bash completion for docker" do
- 	block do
-		if ::File.exists?("/etc/bash_completion.d/docker.io")
-			docker_completion = "/etc/bash_completion.d/docker.io"
-		else
-			docker_completion = "/etc/bash_completion.d/docker"
-		end
-
-  		file = Chef::Util::FileEdit.new(docker_completion)
-  		file.insert_line_if_no_match(/complete -F _docker docker/, "complete -F _docker docker")
-  		file.write_file
-	end
-end
-
-if ['i386', 'i486', 'i586', 'i686', 'x86'].include? node[:kernel][:machine]
-	log 'Running on a x86 architecture. Will install a more recent version of docker to avoid https://github.com/docker/docker/issues/4556'
-
-	execute "echo DOCKER_OPTS=\\\"-H tcp://0.0.0.0:20023 -H unix:///var/run/docker.sock\\\" > /etc/default/docker.io" do
-		not_if { ::File.exists?("/tmp/docker-conf")}
-	end
-
-	execute 'update-rc.d docker.io defaults'
-
-	service 'docker.io' do
-		action :start
-		notifies :run, 'execute[docker pull i686/ubuntu]'
-	end
-
-	execute "apt-get install -y --force-yes libdevmapper1.02.1 dmsetup docker.io"
-
-	execute 'docker pull i686/ubuntu' do
-		action :nothing
-	end
-else
-	execute "echo DOCKER_OPTS=\\\"-H tcp://0.0.0.0:20023 -H unix:///var/run/docker.sock\\\" > /etc/default/docker.io" do
-		not_if { ::File.exists?("/tmp/docker-conf")}
-	end
-
-	execute 'update-rc.d docker.io defaults'
-
-	service 'docker.io' do
-		action :start
-		notifies :run, 'execute[docker pull ubuntu:14.04]'
-	end
-
-	execute 'docker pull ubuntu:14.04' do
-		action :nothing
-	end
-end
-
-# Secure docker service. Allow access only from CI master
-execute 'iptables -F'
-execute 'iptables -A INPUT -p tcp --dport 20023 -s ci.kurento.org -j ACCEPT'
-execute 'iptables -A INPUT -p tcp --dport 20023 -j DROP'
+# Install docker-compose
+execute 'curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose'
+execute 'chmod +x /usr/local/bin/docker-compose'
+execute "curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
