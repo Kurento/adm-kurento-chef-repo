beae26e94441e86685ff2a3b91b9fb0a4d2dd0a4
gortazar <patxi.gortazar@gmail.com>
2016-01-18 14:30:25 +0100

    [docker] Fix error on curl
    Change-Id: I117c48ced95fa641f3b44478a594e12e70991fbe

--- cookbooks/kurento/recipes/docker.rb
+++ cookbooks/kurento/recipes/docker.rb
@@ -42,8 +42,7 @@ service "docker" do
 end
 
 # Install docker-compose
-execute 'curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose'
+execute 'curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 execute 'chmod +x /usr/local/bin/docker-compose'
 execute "curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
