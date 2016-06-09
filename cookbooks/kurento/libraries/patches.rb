c5643b7354ae3bb9ec4c36a42421f493c6b0a31e
Javier Lopez Fernandez <fjlopez@kurento.org>
2016-06-09 15:22:42 +0200

    [Fix bug] fix wrong signature in function run_noninteractive
    Change-Id: I65333c1c4651160970bcac5936f46d81068d64af

--- cookbooks/kurento/libraries/patches.rb
+++ cookbooks/kurento/libraries/patches.rb
@@ -24,8 +24,8 @@ class ::Chef::Provider::Package::Rubygems
 end
 
 class ::Chef::Provider::Package::Apt
-  def run_noninteractive(command)
+  def run_noninteractive(*args)
     # There are some mighty big packages in this recipe, and 600s is just not enough!
-    shell_out!(command, :env => { "DEBIAN_FRONTEND" => "noninteractive", "LC_ALL" => nil }, :log_level => :info, :timeout => 216000)
+    shell_out!(a_to_s(*args), :env => { "DEBIAN_FRONTEND" => "noninteractive", "LC_ALL" => nil }, :log_level => :info, :timeout => 216000)
   end
 end
