50e2d7b910de70b95da014c4f79ee19db36c2cf1
Fede Diaz <nordri@gmail.com>
2017-05-24 12:19:28 +0200

    Updated version

--- cookbooks/kurento/metadata.rb
+++ cookbooks/kurento/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'patxi.gortazar@gmail.com'
 license          'LGPL 2.1'
 description      'Installs/Configures kurento'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '6.2.2'
+version          '6.2.3'
 
 depends 'git_user'
 depends 'apt', ">= 2.0.0"
